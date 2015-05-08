5b0378d854358bc4b97835a01162bd69abf425c9
Christopher Tarazi <christarazi@users.noreply.github.com>
2015-05-08 16:52:11 -0700

    Update happy.py

--- happy.py
+++ happy.py
@@ -1,33 +1,33 @@
 import sys
 
 if len(sys.argv) != 2:
-	print("usage: " + sys.argv[0] + " <integer>")
-	exit(1)
+  print("usage: " + sys.argv[0] + " <integer>")
+  exit(1)
 
 try:
-	n = int(sys.argv[1])
+  n = int(sys.argv[1])
 except ValueError:
-	print("input is not an integer")
-	exit(2)
+  print("input is not an integer")
+  exit(2)
 
 if n <= 0:
-	print("input must be greater than 0")
-	exit(3)
+  print("input must be greater than 0")
+  exit(3)
 
 def happy(n):
-	l = []
+  l = []
+  sum = 0
+  for digit in str(n):
+	sum += int(digit)**2
+  l.append(sum)
+  for i in l:
 	sum = 0
-	for digit in str(n):
-		sum += int(digit)**2
+	for digit in str(i):
+	  sum += int(digit)**2
 	l.append(sum)
-	for i in l:
-		sum = 0
-		for digit in str(i):
-			sum += int(digit)**2
-		l.append(sum)
-		if sum == 1:
-			return "happy number"
-		if l.count(i) > 1:
-			return "unhappy number"
+	if sum == 1:
+	  return "happy number"
+	if l.count(i) > 1:
+	  return "unhappy number"
 
 print(happy(n))
